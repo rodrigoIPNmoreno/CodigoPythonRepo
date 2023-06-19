805ea39ba1fe3df6cb530a8b4e3d53de143a32d4
rodrigoIPNmoreno <daft1077@hotmail.com>
2023-06-19 14:15:26 -0600

    Commit inicial redes neuronales 2da fase

---
+++ redNeuronal.py
@@ -0,0 +1,9 @@
+#PARTE 1 CONSTRUIR MODELO DE CNN
+
+#IMPORTAR LAS LIBRERIAS Y PAQUETES
+
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten #aplanar 
+from keras.layers import Dense #crear capas interconectadas 
